0af905f8e09f806341401380fb6f7bc6d40314ab
ahuguet <andreu.huguet@estudiantat.upc.edu>
2021-02-20 13:29:51 +0100

    Map link change

--- map/map.js
+++ map/map.js
@@ -2,6 +2,6 @@ const islands = document.getElementsByClassName("island");
 
 Array.from(islands).forEach(function(island) {
     island.addEventListener('click', () => {
-        window.location.href = "../index.html";
+        window.location.href = "../index.html?folder=0&part=1";
     });
 });
